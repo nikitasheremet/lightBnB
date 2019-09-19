64bcc1529b526aa21575b5460d19f933e1fee582
nikitasheremet <nikita.sheremet253@gmail.com>
2019-09-19 15:51:14 +0000

    add functionality to be able to search within the databse

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -90,13 +90,59 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  return pool.query(`
-    SELECT *
-    FROM properties
-    LIMIT $1
-  `,[limit])
-  .then((res) => res.rows)
-  .catch((e) => console.log(e));
+  // console.log(options.minimum_price_per_night);
+  // console.log(options.maximum_price_per_night);
+  // console.log(options)
+  // console.log(limit);
+  // console.log(options.minimum_rating);
+  // 1
+  const queryParams = [];
+  // 2
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
+
+  // 3
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length} `;
+  }
+  if (options.owner_id) {
+    queryParams.push(options.owner_id);
+    queryString += `\nWHERE owner_id = $${queryParams.length} `;
+  }
+  if (options.minimum_price_per_night) {
+    queryParams.push(options.minimum_price_per_night);
+    queryString += `\nWHERE cost_per_night >= $${queryParams.length}`;
+  }
+  if (options.maximum_price_per_night) {
+    queryParams.push(options.maximum_price_per_night);
+    queryString += `\nWHERE cost_per_night <= $${queryParams.length}`;
+  }
+  queryString += `\nGROUP BY properties.id`;
+
+  if (options.minimum_rating) {
+    queryParams.push(options.minimum_rating);
+    queryString += `\nHAVING avg(property_reviews.rating) >= $${queryParams.length} `;
+  }
+
+  // 4
+  queryParams.push(limit);
+  queryString += `
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+  // 5
+  queryString = queryString.replace(/WHERE/gi,'AND').replace(/AND/, 'WHERE');
+  console.log(queryString, queryParams);
+
+  // 6
+
+  return pool.query(queryString, queryParams)
+  .then(res => res.rows);
 }
 exports.getAllProperties = getAllProperties;
 
